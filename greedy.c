fecb9520b9e274fb73ce87d8b5bd88df922b87d8
AndrewVanB <andrewvanbelleghem@gmail.com>
2017-02-13 12:24:31 -0500

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -4,9 +4,9 @@
 bool inputValid = false;
 bool debug = false;
 float input;
-float quarter = 0.25f, dime = 0.10f, nickle = 0.05f, penny = 0.01f;
-float change;
-float numberOfCoins;
+int quarter = 25, dime = 10, nickle = 5, penny = 1;
+int cents;
+int numberOfCoins;
 
 void inputCheck(void);
 void calculateChange(float amount);
@@ -68,60 +68,26 @@ void inputCheck(void) {
 /*
     ============calculateChange Functoin===============
     
-    Makes the float value 'change' equal 'amount' which is the same as input, 
-    since input was the parameter.
+    Converts the 'amount' value to an integer, cents now represents 0.41 as 41
     
-    If 'change' is greater than '0' run through this block and loop until 'change' is less than '0'.
     
-        if 'quarter' (0.25f set at the top) is less than or equal to 'change', get the 'change' value, 
-        and negate the 'quarter' (0.25f) value from the 'change' value and then increment 'numberOfCoins' by 1.
-        
-        else if 'dime' (0.10f set at the top) is less than or equal to 'change', get the 'change' value, 
-        and negate the 'dime' (0.10f) value from the 'change' value and then increment 'numberOfCoins' by 1.
-        
-        else if 'nickle' (0.05f set at the top) is less than or equal to 'change', get the 'change' value, 
-        and negate the 'dime' (0.05f) value from the 'change' value and then increment 'numberOfCoins' by 1.
-        
-        else if 'penny' (0.01f set at the top) is less than or equal to 'change', get the 'change' value, 
-        and negate the 'penny' (0.01f) value from the 'change' value and then increment 'numberOfCoins' by 1.
-        
-        else if 'change' is greater than 0.00 but less than 0.0090, set 'change' to 0.00f, resulting in no
-        floatpoint inaccuracy.
-        
-        Once loop is complete, print the 'numberOfCoins' value, and ignores the numbers after the decimal palce.
 */
 
 void calculateChange(float amount) {
-    change = amount;
+    cents = amount * 1000 / 10;
     
-    
-    for(;change > 0.00f;) {
-        
-        if(quarter <= change) {
-                                                            if(debug == true) printf("%.50f - %.2f\n", change, quarter);
-            change -= quarter;
-            numberOfCoins++;
-            
-        } else if(dime <= change) {
-                                                            if(debug == true) printf("%.50f - %.2f\n", change, dime);
-            change -= dime;
-            numberOfCoins++;
-            
-        } else if(nickle <= change) {
-                                                            if(debug == true) printf("%.50f - %.2f\n", change, nickle);
-            change -= nickle;
-            numberOfCoins++;
-            
-        } else if(change < 0.04f && change >= 0.0090f) {
-                                                            if(debug == true) printf("%.50f - %.2f\n", change, penny);
-            change -= penny;
-            numberOfCoins++;
-            
-        } else if (change >= 0.00f && change < 0.0090f) {
-                                                            if(debug == true) printf("Float point incaccuraccy detected, setting change to 0\n");
-            change = 0.00f;
+    for(;cents > 0;) {
+        if(cents >= quarter) {
+            cents -= quarter;
+        } else if(cents >= dime) {
+            cents -= dime;
+        } else if(cents >= nickle) {
+            cents -= nickle;
+        } else if(cents >= penny) {
+            cents -= penny;
         }
+        numberOfCoins++;
+        printf("Cents Left: %i\n", cents);
     }
-                                                            if(debug == true) printf("Change: %0.50f\n", change);
-    printf("%.0f\n", numberOfCoins);
+    printf("%i\n", numberOfCoins);
 }
